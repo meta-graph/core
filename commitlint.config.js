45c1ade8e02b9d9c917368ac27b963e78178a34b
J. Kirby Ross <james@flyingrobots.dev>
2025-10-16 23:33:41 -0700

    ci: allow build commit type

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   rules: {
-    'type-enum': [2, 'always', ['chore', 'ci', 'docs', 'feat', 'fix', 'refactor', 'test']],
+    'type-enum': [2, 'always', ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'refactor', 'test']],
     'type-case': [2, 'always', 'lower-case'],
     'subject-empty': [2, 'never'],
     'header-max-length': [2, 'always', 72]
