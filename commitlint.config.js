2f9d7a940930b46b3a3d76dfbe2cf2d8be31758e
J. Kirby Ross <james@flyingrobots.dev>
2025-10-21 02:07:23 -0700

    ci: skip non-conventional bot commits

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  ignores: [
+    (message) => /^Update\b/.test(message)
+  ],
   rules: {
     'header-max-length': [2, 'always', 72]
   }
